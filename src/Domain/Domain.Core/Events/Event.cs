5b7c60b27d4554d23871eb6758418b81c91d0219
sinantok <sinantok@outlook.com>
2020-10-25 19:39:08 +0300

    Initial commit of Domain.Core project

---
+++ src/Domain/Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
