fabe43757b1e01601d53c6c20fca233667a0ba08
sinantok <sinantok@outlook.com>
2020-10-29 22:25:57 +0300

    rabbitMq publish operations

--- src/Infra.Bus/Bus.RabbitMQ/RabbitMQBus.cs
+++ src/Infra.Bus/Bus.RabbitMQ/RabbitMQBus.cs
@@ -1,6 +1,10 @@
 ﻿using Domain.Core.Bus;
 using Domain.Core.Commands;
 using Domain.Core.Events;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,23 +14,55 @@ namespace Bus.RabbitMQ
 {
     public class RabbitMQBus : IEventBus, IDisposable
     {
-        public RabbitMQBus()
+        private readonly IMediator _mediator;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+        //
+
+        private static ConnectionFactory factory;
+        private static IConnection connection;
+        private static IModel channel;
+
+
+        public RabbitMQBus(IMediator mediator, IServiceScopeFactory serviceScopeFactory)
         {
+            _mediator = mediator;
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            channel?.Dispose();
+            connection?.Dispose();
         }
 
         public void Publish<T>(T @event) where T : Event
         {
-            throw new NotImplementedException();
+            if (connection == null)
+            {
+                factory = new ConnectionFactory() { HostName = "localhost" };
+                connection = factory.CreateConnection();
+                factory.AutomaticRecoveryEnabled = true;
+                factory.TopologyRecoveryEnabled = true;
+                factory.NetworkRecoveryInterval = TimeSpan.FromSeconds(5);
+                factory.UseBackgroundThreadsForIO = true;
+                factory.RequestedHeartbeat = TimeSpan.FromSeconds(3);
+                channel = connection.CreateModel();
+                var prop = channel.CreateBasicProperties();
+                prop.DeliveryMode = 2;
+            }
+            var eventName = @event.GetType().Name;
+            channel.QueueDeclare(eventName, false, false, false, null);
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", eventName, null, body);
         }
 
         public Task SendCommand<T>(T command) where T : Command
         {
-            throw new NotImplementedException();
+            return _mediator.Send(command);
         }
 
         public void Subscribe<T, TH>()
