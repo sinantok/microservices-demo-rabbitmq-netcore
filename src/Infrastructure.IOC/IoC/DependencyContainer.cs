242883631f77a238dff09b1f4a56412fdc68b5b1
sinantok <sinantok@outlook.com>
2020-11-01 15:19:09 +0300

    dependecyContainer created

--- src/Infrastructure.IOC/IoC/DependencyContainer.cs
+++ src/Infrastructure.IOC/IoC/DependencyContainer.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Bus.RabbitMQ;
+using Domain.Core.Bus;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace IoC
 {
     public class DependencyContainer
     {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+        }
     }
 }
