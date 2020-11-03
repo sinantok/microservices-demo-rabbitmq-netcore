f5d5ab3b9d404c16f194b549e98c181249b3ba1e
sinantok <sinantok@outlook.com>
2020-11-03 23:23:20 +0300

    swagger configured on bankApi

--- src/Microservices/Banking/Api/Banking.Api/Startup.cs
+++ src/Microservices/Banking/Api/Banking.Api/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Banking.Data.Context;
 using IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Banking.Api
 {
@@ -29,11 +24,18 @@ namespace Banking.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BankingDbContext>(options =>
-            {                         
+            {
                 options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
             });
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
+            });
+
+            services.AddMediatR(typeof(Startup));
 
             RegisterServices(services);
 
@@ -57,6 +59,12 @@ namespace Banking.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
