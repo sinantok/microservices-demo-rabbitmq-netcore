4ff5f7afea74cad458c2ee3c2fd4a0a0dcbdaeee
sinantok <sinantok@outlook.com>
2020-11-22 18:59:38 +0300

    transferDb migration operations

---
+++ src/Microservices/Transfer/Data/Transfer.Data/Migrations/20201122155745_First_Migration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transfer.Data.Migrations
+{
+    public partial class First_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransferLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromAccount = table.Column<int>(type: "int", nullable: false),
+                    ToAccount = table.Column<int>(type: "int", nullable: false),
+                    TransferAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransferLogs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransferLogs");
+        }
+    }
+}
